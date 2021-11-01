81db791b9df5ae3abf43024fe1885b223bea6bbf
Risu Singh <risusingh35@gmail.com>
2021-11-01 23:25:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+//static file access
+app.use(express.static("publice"));
+app.use("/css", express.static(__dirname + "public/css"));
+app.use("/img", express.static(__dirname + "public/img"));
+app.use("/js", express.static(__dirname + "public/js"));
+
+app.get("/", (req, res) => {
+  res.send("Hello RK Singh");
+});
+app.set("view", "./src/views");
+app.set("view engine", "ejs");
+
+//port Listen on 3000
+const port = 3000;
+app.listen(port, () => console.log("Listening on port 3000"));
